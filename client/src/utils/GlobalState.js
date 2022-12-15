238d9ebe070954cfe9d71a722981ac7bcbfad378
hotsoup42 <jose22gw@gmail.com>
2022-12-14 16:29:05 -0800

    refactored react globalState to redux. import'ed react-redux {provider}, moved initial state to reducer.js

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,14 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+import React from "react";
+import { Provider } from 'react-redux'
+// Import Redux store from the file we created in /utils
+import store from './store';
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
-
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer ({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
-
-  return <Provider value={[state, dispatch]} {...props} />;
+export default function StoreProvider(props) { 
+// Return the provider component provided by react-redux
+  return <Provider store = {store} {...props} />;
 };
 
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
 
-export { StoreProvider, useStoreContext };
+
+
+  
