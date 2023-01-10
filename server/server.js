3cc166e6285a2d115b8ead9ad6158ebb496854ff
hotsoup42 <jose22gw@gmail.com>
2023-01-10 14:43:34 -0800

    had package version mismatches, is now fixed and working.

--- server/server.js
+++ server/server.js
@@ -8,11 +8,16 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
+// // added this line : Error: You must `await server.start()`
+//   await server.start();
+
+
 
 server.applyMiddleware({ app });
 
